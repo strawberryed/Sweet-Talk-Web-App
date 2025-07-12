9d8a5599a4e2df40a244d5c1707209a5768928fc
strawberryed <abbychia12@gmail.com>
2025-07-12 14:12:48 +0800

    Delete urls.py

--- urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import confirmation_view  # Import the view
-
-urlpatterns = [
-    path('confirmation/', confirmation_view, name='confirmation'),  # URL for the confirmation page
-]
